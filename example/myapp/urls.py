e786da43c42a8539aea9a0216cbb13528105b33c
Marcus Erlandsson <mail@marquz.com>
2013-10-09 16:27:48 +0200

    Added goal tracking to the example app

--- example/myapp/urls.py
+++ example/myapp/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns(
     '',
     url(r'^sample/$', 'myapp.views.sample', name='myapp_sample'),
+    url(r'^goalie/$', 'myapp.views.goalie', name="myapp_goalie"),
 )
